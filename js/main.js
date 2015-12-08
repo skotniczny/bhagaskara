2cb552ce10cf7b9aecf786b64016f8c495a1daa4
Lukasz <skotniczny@outlook.com>
2015-12-08 10:30:07 +0100

    dodanie hamburger menu i filtrowania w sekcji portfolio

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-	var menu = document.querySelector(".main-menu");
-	menu.classList.add('hidden');
+  function $(selector) {
+    return (selector.charAt(0) == "#") ? document.querySelector(selector) : document.querySelectorAll(selector);
+  }
 
-	var btn = document.querySelector("#hamburger");
-	btn.addEventListener("click", function(e) {
-		e.preventDefault();
-		console.log(this.parent);
-		this.parentElement.nextElementSibling.classList.toggle('hidden');
-	});
-});
+  var menu = $(".main-menu")[0];
+  menu.classList.add('hidden');
+
+  var btn = $("#hamburger");
+  btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    this.parentElement.nextElementSibling.classList.toggle("hidden");
+  }, false);
+
+  var allBtn = $("#all");
+  var webBtn = $("#web");
+  var appsBtn = $("#apps");
+  var iconsBtn = $("#icons");
+
+  function filterPortfolio(event) {
+    var portfolioElms = $(".portfolio .row div");
+    var filter = event.target.id;
+    for (var i = 0; i < portfolioElms.length; i++) {
+      portfolioElms[i].classList.remove('hidden');
+      var elmData = portfolioElms[i].getAttribute("data-cat");
+      // console.log(filter, elmData);
+      if (filter !== elmData && filter !== "all") {
+        portfolioElms[i].classList.add("hidden");
+      }
+    }
+  }
+
+  allBtn.addEventListener("click", filterPortfolio, false);
+  webBtn.addEventListener("click", filterPortfolio, false);
+  appsBtn.addEventListener("click", filterPortfolio, false);
+  iconsBtn.addEventListener("click", filterPortfolio, false);
+}, false);
