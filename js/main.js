73e258411f45456ebaadd6e7ff1a76999491a18e
Lukasz <skotniczny@outlook.com>
2015-12-09 16:08:10 +0100

    dodanie slajdera w sekcji blog

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-  function $(selector) {
+  function _(selector) {
     return (selector.charAt(0) == "#") ? document.querySelector(selector) : document.querySelectorAll(selector);
   }
 
-  var menu = $(".main-menu")[0];
+  var menu = _(".main-menu")[0];
   menu.classList.add('hidden');
 
-  var btn = $("#hamburger");
+  var btn = _("#hamburger");
   btn.addEventListener("click", function(e) {
     e.preventDefault();
     this.parentElement.nextElementSibling.classList.toggle("hidden");
   }, false);
 
-  var allBtn = $("#all");
-  var webBtn = $("#web");
-  var appsBtn = $("#apps");
-  var iconsBtn = $("#icons");
+  var allBtn = _("#all");
+  var webBtn = _("#web");
+  var appsBtn = _("#apps");
+  var iconsBtn = _("#icons");
 
   function filterPortfolio(event) {
-    if($("button.active")[0]) {
-      $("button.active")[0].classList.remove("active");
+    if(_("button.active")[0]) {
+      _("button.active")[0].classList.remove("active");
     }
     this.classList.add("active");
-    var portfolioElms = $(".portfolio .row div");
+    var portfolioElms = _(".portfolio .row div");
     var filter = event.target.id;
     for (var i = 0; i < portfolioElms.length; i++) {
       portfolioElms[i].classList.remove("hidden");
@@ -38,4 +38,15 @@ document.addEventListener("DOMContentLoaded", function() {
   webBtn.addEventListener("click", filterPortfolio, false);
   appsBtn.addEventListener("click", filterPortfolio, false);
   iconsBtn.addEventListener("click", filterPortfolio, false);
+
+
+  $(".slider-indicators").on("click", "li", function(event) {
+    $(this).addClass("active").siblings().removeClass("active");
+    var itemIndex = $(this).index();
+    var slidePosition = itemIndex * 970;
+    var left = -slidePosition;
+    // console.log("slidePosition", slidePosition);
+    // console.log(".stripe left:", left);
+    $(".slider-stripe").animate({"left": left});
+  });
 }, false);
